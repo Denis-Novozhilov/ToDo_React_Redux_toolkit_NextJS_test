27b796fb3549d16fb91fd519e50492b689917262
Denis Matyukha <denmatuhawork@gmail.com>
2022-02-27 23:35:23 +0300

    Final version with cleared validation functions and a bit of responsive styles

--- components/ToDoList/ToDoList.js
+++ components/ToDoList/ToDoList.js
@@ -1,11 +1,12 @@
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import {
     addTaskToList,
     removeTaskFromList,
     addToCheckedTasks,
-    removeFromCheckedTasks
+    removeCheckedTasks
 } from '../../toolkitRedux/toolkitSlice';
+
 import { v4 as uuidv4 } from 'uuid';
 import s from '../../styles/ToDoList.module.scss';
 import cn from 'classnames';
@@ -14,60 +15,57 @@ const InputField = () => {
 
     const tasksList = useSelector(state => state.toolkit.tasksList);
     const checkedTasks = useSelector(state => state.toolkit.checkedTasks);
-    const dispatch = useDispatch();
 
-    const [validity, setValidity] = useState(false);
+    const dispatch = useDispatch();
     const inputRef = useRef();
 
-    const onAddTasksHandler = (newTask) => {
+    const isPressedKeyCode = (e, btnName, btnCode) => (
+        ((e.code === btnName) || 
+         (e.key === btnName) || 
+         (e.keyCode === btnCode)) ? true : false
+    );
+    
+    const onKeyUpHandler = (e) => {
+        const elem = inputRef.current;
+        elem.setCustomValidity('');
+        
+        // validation to prevent symbols 'Space' at the beginning
+        const re = /^ {1,}$/;
+
+        if (elem.value && re.test(elem.value)) {
+            elem.setCustomValidity('Don\'t use \'Space\' at the beginning.');
+            elem.reportValidity();
+            elem.value = '';
+        };
+
+        // addition not empty task after Enter button pressed 
+        if (elem.value && (isPressedKeyCode(e, 'Enter', 13))) {
+            onAddTasksHandler(elem.value, elem);
+        };
+    };
+
+    const onAddTasksHandler = (newTask, elem) => {
         // prevent adding the same tasks and empty string
         if (newTask && tasksList.includes(newTask)) {
-            inputRef.current.setCustomValidity('Task already exist');
-            setValidity(false);
-            inputRef.current.reportValidity();
+            elem.setCustomValidity('Task already exist');
+            elem.reportValidity();
+            elem.focus();
         } else {
             newTask && dispatch(addTaskToList(newTask));
-            inputRef.current.value = '';
-            inputRef.current.focus();
+            elem.value = '';
+            elem.focus();
         }
     };
 
     const onRemoveTasksHandler = (task) => {
         dispatch(removeTaskFromList(task));
-        dispatch(removeFromCheckedTasks(task));
+        dispatch(removeCheckedTasks(task));
     };
-
 
     const toggleCheckedTasksHandler = (task) => {
         checkedTasks.includes(task) ?
-            dispatch(removeFromCheckedTasks(task)) :
+            dispatch(removeCheckedTasks(task)) :
             dispatch(addToCheckedTasks(task));
-    };
-
-    const onKeyUpHandler = (e) => {
-
-        // validation to prevent symbols 'Space' at the beginning
-        const el = inputRef.current;
-        const re = /^ {1,}$/;
-
-        if (el.value && re.test(el.value)) {
-            el.setCustomValidity('Don\'t use \'Space\' at the beginning.');
-            setValidity(false);
-            el.reportValidity();
-            el.value = '';
-
-        // prevention wrong positive validation after navigation via Tab button
-        } else if ((e.code !== "Tab") || (e.key !== "Tab") || (e.keyCode !== 9)) {
-
-            // make the printed message able to be added to tasks  
-            el.setCustomValidity('');
-            setValidity(true);
-        };
-
-        // addition not empty task after Enter button pressed 
-        if (el.value && ((e.code === 'Enter') || (e.key === 'Enter') || (e.keyCode === 13))) {
-            onAddTasksHandler(el.value);
-        };
     };
 
     return (
@@ -76,9 +74,9 @@ const InputField = () => {
 
                 <input
                     className={cn(s.taskInputBox, s.cornerBorder)}
-                    onKeyUp={onKeyUpHandler}
+                    onKeyUp={(e) => onKeyUpHandler(e)}
                     placeholder="write a new task"
-                    maxLength="45"
+                    maxLength="40"
                     minLength="1"
                     tab-index="1"
                     type="text"
@@ -88,28 +86,28 @@ const InputField = () => {
                     ref={inputRef} />
 
                 <button
-                    className={cn(s.taskInputButton, s.cornerBorder, { [s.hidden]: !validity })}
-                    onClick={() => onAddTasksHandler(inputRef.current.value)}>
+                    className={cn(s.taskInputButton, s.cornerBorder)}
+                    onClick={() => onAddTasksHandler(inputRef.current.value, inputRef.current)}
+                >
                     Enter
                 </button>
 
             </div>
 
-            <div className={cn(s.container)}>
+            <div
+                className={cn(s.container)}>
 
                 {tasksList.map((task) => (
 
-                    task && <div key={uuidv4()}
+                    task && <div
+                        key={uuidv4()}
                         className={cn(s.containerTask, s.cornerBorder, { [s.chekedCondition]: checkedTasks.includes(task) })}>
 
                         <input className={cn(s.containerCheckBox)}
                             tab-index='1'
                             onClick={() => toggleCheckedTasksHandler(task)}
-                            onKeyDown={(e) => {
-                                if ((e.code === 'Enter') || (e.key === 'Enter') || (e.keyCode === 13)) {
-                                    toggleCheckedTasksHandler(task);
-                                }
-                            }} />
+                            onKeyDown={(e) => { if (isPressedKeyCode(e, 'Enter', 13)) toggleCheckedTasksHandler(task)}} 
+                        />
 
                         <div className={cn(s.task, s.cornerBorder)}>
                             {task}
@@ -120,7 +118,6 @@ const InputField = () => {
                             tab-index='1'
                             value='✖'
                             onClick={() => onRemoveTasksHandler(task)} />
-
                     </div>
 
                 ))}
